28550f9735e3979b86677ce95a79e534a402215d
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-05-28 17:15:17 +0100

    Library tests passing

---
+++ src/main/java/Library.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book> collection;
+    private int capacity;
+
+    public Library(int capacity){
+        this.collection = new ArrayList<Book>();
+        this.capacity = capacity;
+    }
+
+    public int getNumberOfBooks() {
+        return this.collection.size();
+    }
+
+    public void addBookToCollection(Book book){
+        if (this.collection.size() < this.capacity) {
+            this.collection.add(book);
+        }
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,50 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    private Library library;
+    private Book book;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+    private Book book4;
+    private Book book5;
+
+    @Before
+    public void setUp() {
+        library = new Library(5);
+        book = new Book("Shark Drunk", "Morten Stroksnes", "non-fiction");
+        book1 = new Book("Catch 22", "Joseph Heller", "fiction");
+        book2 = new Book("Mog the Forgetful Cat", "Judith Kerr", "childrens");
+        book3 = new Book("Eleanor Oliphant is Completely Fine", "Gail Honeyman", "fiction");
+        book4 = new Book("Eleanor Oliphant is Completely Fine", "Gail Honeyman", "fiction");
+        book5 = new Book("Eleanor Oliphant is Completely Fine", "Gail Honeyman", "fiction");
+
+
+        library.addBookToCollection(book);
+        library.addBookToCollection(book1);
+        library.addBookToCollection(book2);
+        library.addBookToCollection(book3);
+    }
+
+    @Test
+    public void canGetNumberOfBooks(){
+        assertEquals(4, library.getNumberOfBooks());
+    }
+
+    @Test
+    public void canAddBookToCollectionIfRoom(){
+        library.addBookToCollection(book4);
+        assertEquals(5, library.getNumberOfBooks());
+    }
+
+    @Test
+    public void cantAddBookToCollectionIfFull(){
+        library.addBookToCollection(book4);
+        library.addBookToCollection(book5);
+        assertEquals(5, library.getNumberOfBooks());
+    }
+}
